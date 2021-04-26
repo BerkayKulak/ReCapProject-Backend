872820a2d7721e490298358670ea0d70bf5234ff
Berkay Kulak <beko_468@hotmail.com>
2021-04-26 14:49:26 +0300

    26-04

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -29,7 +30,7 @@ namespace WebAPI.Controllers
         {
             // Swagger
             // dependency chain--
-
+            Thread.Sleep(5000);
             var result = _carService.GetAll();
             if (result.Success)
             {
